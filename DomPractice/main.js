f0ca2ae73a41be6dcbfc49d33b1cfc45bfde6cad
Tejas Patil <tejaspatil159@users.noreply.github.com>
2025-07-22 01:00:32 +0530

    1. Added js for addind value in table 2. Added validation message 3. prevent multiple data adding

--- DomPractice/main.js
+++ DomPractice/main.js
@@ -1,11 +1,63 @@
-function submitForm(e){
-    debugger
-    e.preventDefault();
-    let Name= document.querySelector('#name'); 
+let nameField = document.getElementById('name');
+let tableBody = document.getElementById('tableBody');
+let srNo = 0
+let required = document.querySelector('required');
 
-    addToTable(Name.value)
+document.addEventListener('submit', submitForm)
+
+function submitForm(e) {
+    e.preventDefault()
+    if (nameField.value !== '') {
+        addToTable()
+    } else {
+        AddValidationMessage()
+    }
 }
 
-function addToTable(){
+function addToTable() {
+    srNo += 1;
+    let tr = document.createElement('tr')
+
+    let tdOfSrNo = document.createElement('td')
+    tdOfSrNo.classList = "tdItems"
+
+    tdOfSrNo.appendChild(document.createTextNode(srNo))
+
+    let tdOfName = document.createElement('td')
+    tdOfName.classList = "tdItems"
+
+    tdOfName.appendChild(document.createTextNode(nameField.value))
+
+    let tdOfAction = document.createElement('td')
+    tdOfAction.classList = "tdItems"
+
+
+
+    tr.appendChild(tdOfSrNo)
+    tr.appendChild(tdOfName)
+    tr.appendChild(tdOfAction)
+
+    tableBody.appendChild(tr)
+
+    clearField()
 
 }
+
+function clearField() {
+    nameField.value = ''
+}
+
+function AddValidationMessage() {
+
+    const nextSibling = nameField.nextElementSibling
+    if (nextSibling && nextSibling.classList('required')) {
+        return
+    } else {
+        let p = document.createElement('p')
+        p.classList = 'required'
+        p.appendChild(document.createTextNode("Name is Required"))
+        nameField.after(p)
+    }
+
+}
+
